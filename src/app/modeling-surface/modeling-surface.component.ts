6442d777bd34bffebf7b0566644ecf00d3476581
Michael Zemler <michael.zemler@hotmail.com>
2019-10-29 19:38:46 +0100

    starting to implement adding custom elements to surface

--- src/app/modeling-surface/modeling-surface.component.ts
+++ src/app/modeling-surface/modeling-surface.component.ts
@@ -1,18 +1,19 @@
 import { Component, OnInit, Input, ViewChild, ComponentFactoryResolver} from '@angular/core';
 
-// contains view ref
+// contains view ref, adds elements to main-editor container
 import { ElementDirective } from '../main-editor/element-container.directive';
 
 import { BpmnElement } from '../bpmn-element/bpmn-element';
 
 import { BpmnElementComponent } from '../bpmn-element/bpmn-element.component';
 
+import { BpmnElementsService } from '../bpmn-element/bpmn-elements.service';
+
 
 @Component({
   selector: 'app-modeling-surface',
   template: `
           <div class = "elements">
-            <p>einzelne elemente</p>
             <ng-template elements-host></ng-template>
             <button (click) = "loadComponent()"></button>
           </div>
@@ -21,8 +22,8 @@ import { BpmnElementComponent } from '../bpmn-element/bpmn-element.component';
 })
 export class ModelingSurfaceComponent implements OnInit {
   @Input() bpmnElements: BpmnElement;
-  @ViewChild(ElementDirective, {static: true})adHost: ElementDirective;
-  constructor(private componentFactoryResolver: ComponentFactoryResolver) { }
+  @ViewChild(ElementDirective, {static: true}) elementsHost: ElementDirective;
+  constructor(private componentFactoryResolver: ComponentFactoryResolver, private elementsService: BpmnElementsService) { }
 
   ngOnInit() {
     this.loadComponent();
@@ -33,10 +34,20 @@ export class ModelingSurfaceComponent implements OnInit {
 
     const componentFactory = this.componentFactoryResolver.resolveComponentFactory(adItem.component);
 
-    const viewContainerRef = this.adHost.viewContainerRef;
+    const viewContainerRef = this.elementsHost.viewContainerRef;
 
     const componentRef = viewContainerRef.createComponent(componentFactory);
-    (<ElementDirective>componentRef.instance).data = adItem.data;
+    // (<ElementDirective>componentRef.instance).data = adItem.data;
+  }
+
+  loadCustomComponent() {
+    const adItem = this.elementsService.getElements();
+
+    const componentFactory = this.componentFactoryResolver.resolveComponentFactory(adItem.component);
+
+    const viewContainerRef = this.elementsHost.viewContainerRef;
+
+    const componentRef = viewContainerRef.createComponent(componentFactory);
   }
 
 
